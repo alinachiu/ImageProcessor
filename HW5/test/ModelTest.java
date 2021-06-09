763fd8d3609b10d22aec65d7dff9205b72ad58bb
alinachiu <43102414+alinachiu@users.noreply.github.com>
2021-06-09 15:29:51 -0700

    Add files via upload

---
+++ HW5/test/ModelTest.java
@@ -0,0 +1,12 @@
+
+// TODO:
+//  - Alina: colorTransformation, exports, pixel class, test model equivalents
+//    (make sure to test edge cases like clamping, null, IOExceptions, non-existent file/filename,
+//    for pixel check to make sure initialization is correct for rgb, x, y), InputFileManager,
+//    README, UML diagram
+//  - Jessica: filter, checkerboard, image class, imageUtil, InputFilenameManager
+//    
+
+public class ModelTest {
+
+}
