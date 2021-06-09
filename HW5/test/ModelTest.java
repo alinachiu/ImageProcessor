4ba08cde98409dd26e6100b68fd0423e2afd89ce
alinachiu <43102414+alinachiu@users.noreply.github.com>
2021-06-09 15:35:46 -0700

    Update ModelTest.java

--- HW5/test/ModelTest.java
+++ HW5/test/ModelTest.java
@@ -4,7 +4,7 @@
 //    (make sure to test edge cases like clamping, null, IOExceptions, non-existent file/filename,
 //    for pixel check to make sure initialization is correct for rgb, x, y), InputFileManager,
 //    README, UML diagram
-//  - Jessica: filter, checkerboard, image class, imageUtil, InputFilenameManager
+//  - Jessica: filter, imports, checkerboard, image class, imageUtil, InputFilenameManager
 //    
 
 public class ModelTest {
