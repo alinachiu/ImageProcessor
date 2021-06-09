f500364fe45c8520e9b4d4ad398d8ad0e6954cca
alinachiu <43102414+alinachiu@users.noreply.github.com>
2021-06-08 17:12:21 -0700

    Add files via upload

---
+++ HW5/src/model/managers/InputFileManager.java
@@ -0,0 +1,34 @@
+package managers;
+
+import java.io.File;
+import model.IImage;
+import model.ImageUtil;
+import model.PPMImage;
+
+/**
+ * Represents a class that manages the given input file and returns the file's
+ * associated image.
+ */
+public class InputFileManager implements IOManager {
+  private final File file;
+
+  /**
+   * Constructs a {@code InputFileManager} object.
+   * 
+   * @param file the given file to be handled.
+   * @throws IllegalArgumentException if the given file is null
+   */
+  public InputFileManager(File file) throws IllegalArgumentException {
+    if (file == null) {
+      throw new IllegalArgumentException("Cannot have a null file.");
+    }
+    this.file = file;
+  }
+
+  @Override
+  public IImage apply() {
+    String filename = file.getName();
+    IImage image = new PPMImage(ImageUtil.readPPM(filename), filename);
+    return image;
+  }
+}
