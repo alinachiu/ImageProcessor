e366ad61fae9b2782f8bc321b19f88ef3deaa802
alinachiu <43102414+alinachiu@users.noreply.github.com>
2021-06-09 23:38:36 -0700

    Delete IImageCreator.java

--- HW5/src/model/IImageCreator.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-/**
- * This interface represents the different operations that an ImageCreator can perform.
- */
-public interface IImageCreator {
-  public IImage createImage();
-}
